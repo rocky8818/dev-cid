f2e8cbc77fabd1fe9676cc09b0f136a98aeaa8ad
rocky8818 <rickyglz.1318@gmail.com>
2024-07-16 17:44:40 -0600

    🚧 - web structure

--- src/lib/devFunctions.ts
+++ src/lib/devFunctions.ts
@@ -1,8 +1,44 @@
+const apiurl = import.meta.env.API_URL;
+
+
 const sendWhaPC = async (message: string) => {
     var whatsappLink = 'https://wa.me/' + '+523335695400' + '?text=' + encodeURIComponent(message);
     return whatsappLink;
   }
 
+  const getProyecto = async (id:string) => {
+    try {
+      const response = await fetch(`${apiurl}oneproject/${id}`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+    }
+  }
+  
+  
+  
+  const getProyectos = async () => {
+    try {
+      const response = await fetch(`${apiurl}getprojects`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+    }
+  }
+
+  const getBanner = async () => {
+    try {
+      const response = await fetch(`${apiurl}getprojects`);
+      const data = await response.json();
+      
+      return data;
+    } catch (error) {
+    }
+  }
+
   export{
-    sendWhaPC
+    sendWhaPC,
+    getProyectos,
+    getProyecto,
+    getBanner
   }
